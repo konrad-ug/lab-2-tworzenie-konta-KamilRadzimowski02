31be9003c8b01f985c71e0cfbbf4cee0d1f3b136
kamil_radzimowski <k.radzimowski.736@studms.ug.edu.pl>
2022-10-20 18:55:39 +0200

    feautre 2 i 3

--- app/Konto.py
+++ app/Konto.py
@@ -1,9 +1,15 @@
 class Konto:
+    WRONG_PESEL = "NIEPRAWIDLOWY_PESEL"
+
     imie = ""
     nazwisko = ""
     saldo = 0
+    pesel = ""
 
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, pesel):
         self.imie = name
         self.nazwisko = surname
-        pass
+        if len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel = self.WRONG_PESEL
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -3,11 +3,20 @@ import unittest
 from ..Konto import Konto
 
 class TestCreateBankAccount(unittest.TestCase):
+    mock_name = "Dariusz"
+    mock_surname = "Januszewski"
+    mock_pesel = "12345678910"
+    starting_balance = 0
+
+    mock_too_short_pesel = "123"
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        pierwsze_konto = Konto(self.mock_name, self.mock_surname, self.mock_pesel)
+        self.assertEqual(pierwsze_konto.imie, self.mock_name, "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, self.mock_surname, "Nazwisko nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.saldo, self.starting_balance, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel, self.mock_pesel, "Nieprawidłowy pesel")
 
-    #tutaj proszę dodawać nowe testy
+    def test_tworzenie_konta_nieprawidlowy_pesel(self):
+        konto = Konto(self.mock_name, self.mock_surname, self.mock_too_short_pesel)
+        self.assertEqual(konto.pesel, konto.WRONG_PESEL, "Brak informacji o zlym peselu")
